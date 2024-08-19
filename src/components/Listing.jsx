24867df1389ff506d0b412a18245cc97558abc1d
dan <dancr.wley@gmail.com>
2024-08-19 16:56:24 -0400

    fix redirects

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -7,8 +7,9 @@ const Listing = ({ listing }) => {
     return (
         <Link to={`/exhibition/${slugify(listing.title)}`} className="listing-container">
             <div className="text-content">
-                <h3>{listing.date}</h3>
+
                 <h2>{listing.title}</h2>
+                <h3>{listing.date}</h3>
 
                 <p>{listing.category}</p>
                 <p>{listing.location}</p>
@@ -16,7 +17,7 @@ const Listing = ({ listing }) => {
             </div>
             <div className="image-content">
                 <img src={listing.images[0]} alt="Art Image"/>
-                {/*<div className="date">{listing.date}</div>*/}
+                <div className="date">{listing.date}</div>
             </div>
         </Link>
     );
