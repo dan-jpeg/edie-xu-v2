3a97b5545bc1994c5e4aa680e65d4ae2560088c3
dan crowley <dancr.wley@gmail.com>
2025-11-04 18:59:45 -0500

    small fix

--- src/components/admin/Login.jsx
+++ src/components/admin/Login.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import SiloHoverButton from "../SiloHoverButton.jsx";
 
 const API_URL =
-  import.meta.env.VITE_API_URL || "https://exu-admin-server.onrender.com";
+  import.meta.env.VITE_API_URL || "https://exu-admin-server.onrender.com/api";
 
 const Login = ({ onLogin }) => {
   const [password, setPassword] = useState("");
